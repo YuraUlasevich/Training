4578938317e6478fdfd6295a72b8a3c9f23f1db1
YuraUlasevich <yura.ulasevich@gmail.com>
2019-04-09 20:58:35 +0300

    Add new Class TrainingClintDB where we would work with database information about client

---
+++ Training/TrainingClientDB.m
@@ -0,0 +1,13 @@
+//
+//  TrainingDB.m
+//  Training
+//
+//  Created by Юра Уласевич on 4/9/19.
+//  Copyright © 2019 Юра Уласевич. All rights reserved.
+//
+
+#import "TrainingClientDB.h"
+
+@implementation TrainingDB
+
+@end
